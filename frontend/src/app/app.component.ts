218f5c793ddcf12e2c6f31810591c9e8c2965b5d
leonardovbdo <odranoelrubi@gmail.com>
2023-08-09 17:22:51 -0300

    Implementação do filtro

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Status } from './enum/status.enum';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, importProvidersFrom } from '@angular/core';
 import { ServerService } from './service/server.service';
 import { BehaviorSubject, Observable, catchError, map, of, startWith } from 'rxjs';
 import { AppState } from './interface/app-state';
@@ -18,6 +18,7 @@ export class AppComponent implements OnInit {
   readonly Status = Status;
 
   private filterSubject = new BehaviorSubject<string>('');
+  private dataSubject = new BehaviorSubject<CustomResponse>(null);
   filterStatus$ = this.filterSubject.asObservable();
 
 
@@ -27,9 +28,41 @@ export class AppComponent implements OnInit {
     this.appState$ = this.serverService.servers$
     .pipe(
       map(response => {
+        this.dataSubject.next(response);
         return { dataState: DataState.LOADED_STATE, appData: response }
       }),
       startWith({ dataState: DataState.LOADING_STATE }),
+      catchError((error: string) => {
+        return of({ dataState: DataState.ERROR_STATE, error })
+      })
+    );
+  }
+
+  pingServer(ipAddress: string): void {
+    this.filterSubject.next(ipAddress);
+    this.appState$ = this.serverService.ping$(ipAddress)
+    .pipe(
+      map(response => {
+        const index = this.dataSubject.value.data.servers.findIndex(server => server.id === response.data.server.id);
+        this.dataSubject.value.data.servers[index] = response.data.server;
+        this.filterSubject.next('');
+        return { dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }
+      }),
+      startWith({ dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }),
+      catchError((error: string) => {
+        this.filterSubject.next('');
+        return of({ dataState: DataState.ERROR_STATE, error })
+      })
+    );
+  }
+
+  filterServers(status: Status): void {
+    this.appState$ = this.serverService.filter$(status, this.dataSubject.value)
+    .pipe(
+      map(response => {
+        return { dataState: DataState.LOADED_STATE, appData: response }
+      }),
+      startWith({ dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }),
       catchError((error: string) => {
         return of({ dataState: DataState.ERROR_STATE, error })
       })
